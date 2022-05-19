c24ee07eb738552d1131e287c413f3972e6603ea
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-20 05:37:14 +0600

    feat: constants

---
+++ 04.constants/constant.go
@@ -0,0 +1,18 @@
+package main 
+import ("fmt")
+
+// Declaring a Constants
+const MAX int = 10000000000 // typed
+const NAME = "Mohi" // nutyped
+
+// Multitple Constant Declaration
+const (
+	MAX_NUMBER int = 600000
+	BOSS = "Boss"
+)
+
+func main() {
+	const PI float32 = 3.14
+
+	fmt.Println(PI)
+}
