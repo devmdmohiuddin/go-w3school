1b192aff04a02207c5c47d234a8ead6e6d609152
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-20 04:47:46 +0600

    feat: comment

---
+++ 02.comments/comment.go
@@ -0,0 +1,13 @@
+package main 
+import ("fmt")
+
+func main() {
+	// Single line comment
+	fmt.Println("Single Line Comment")
+
+	/*
+		Multile line
+		comment
+	*/
+	fmt.Println("Multile Line Comment")
+}
